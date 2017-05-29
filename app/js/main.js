deb6cf7f8c4cdee5b70ca99a5997513752f091fb
Yakov Limanskiy <limanskiy1989@mail.ru>
2017-05-29 18:33:09 +0300

    advancing enemies // beta shot cannon

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,12 @@
 var fieldWidth = 800;
 var fieldHeight = 600;
 var enemies = [];
+var counter = 0;
 
 var distance = 200;
 var speed = 4;
 var stars;
+
 
 var max = 200;
 var xx = [];
@@ -21,7 +23,13 @@ function preload (){
   game.load.image('star', 'img/dust.png');
   game.load.image('aim', 'img/aim.png');
   game.load.image('enemy', 'img/enemy.png');
+  game.load.image('extraSmall', 'img/enemyExtrasmall.png');
+  game.load.image('enemySmall', 'img/enemySmall.png');
+  game.load.image('enemyLarge', 'img/enemyLarge.png');
   game.load.image('cannon', 'img/cannon.png');
+  game.load.image('cannonShot', 'img/cannonShot.png');
+  game.load.image('laser', 'img/laser.png');
+
   game.load.audio('bgMusic', 'audio/bgTreck.mp3');
 }
 
@@ -29,51 +37,70 @@ function create() {
   BGMusic = game.add.audio('bgMusic', "0.2", true);
   BGMusic.onDecoded.add(start, this);
 
-
   let background = game.add.sprite(0, 0, 'space');
 
-  rightCannon = game.add.sprite(fieldWidth - game.cache.getImage('cannon').height, fieldHeight - game.cache.getImage('cannon').height + 40, 'cannon');
-  rightCannon.anchor.setTo(0.1, 0.5);
-  leftCannon = game.add.sprite(0 + game.cache.getImage('cannon').height, fieldHeight - game.cache.getImage('cannon').height + 40, 'cannon');
-  leftCannon.anchor.setTo(0.1, 0.5);
-  // rightCannon.anchor.setTo(0.1, 0.5);
-
+  /*STARS*/
   if (game.renderType === Phaser.WEBGL)
       {
           max = 2000;
       }
-
       var addStart = game.add.spriteBatch();
-
       stars = [];
-
       for (var i = 0; i < max; i++)
       {
           xx[i] = Math.floor(Math.random() * 800) - 400;
           yy[i] = Math.floor(Math.random() * 600) - 300;
           zz[i] = Math.floor(Math.random() * 1700) - 100;
-
           var star = game.make.sprite(0, 0, 'star');
           star.anchor.set(0.5);
-
           addStart.addChild(star);
-
           stars.push(star);
       }
 
+  weapon();
 
   game.time.events.loop(1000, enemy, this);
   game.time.events.loop(5000, clearDeadEnemies, this);
 }
 
+function weapon (){
+  rightCannon = game.add.sprite(fieldWidth - game.cache.getImage('cannon').height, fieldHeight - game.cache.getImage('cannon').height + 40, 'cannon');
+  rightCannon.anchor.setTo(0.1, 0.5);
+  leftCannon = game.add.sprite(0 + game.cache.getImage('cannon').height, fieldHeight - game.cache.getImage('cannon').height + 40, 'cannon');
+  leftCannon.anchor.setTo(0.1, 0.5);
+  game.input.onDown.add(changeTexture, this);
+  function changeTexture (){
+    if (rightCannon.key === 'cannon'){
+      rightCannon.loadTexture('cannonShot', 0, false);
+      leftCannon.loadTexture('cannonShot', 0, false);
+    }
+   else{
+     rightCannon.loadTexture('cannon', 0, false);
+     leftCannon.loadTexture('cannon', 0, false);
+   }
+  }
+}
+
 function enemy (){
-  var mx = game.width - game.cache.getImage('enemy').width;
-  var my = game.height - game.cache.getImage('enemy').height - 150;
-  let sprite = game.add.sprite(game.rnd.integerInRange(0, mx), game.rnd.integerInRange(0, my), 'enemy');
+  let mx = game.width - game.cache.getImage('enemyLarge').width;
+  let my = game.height - game.cache.getImage('enemyLarge').height - 150;
+
+  let sprite = game.add.sprite(game.rnd.integerInRange(0, mx), game.rnd.integerInRange(0, my), 'extraSmall');
   enemies.push(sprite);
+  function smallTex(){
+    sprite.loadTexture('enemySmall');
+  }
+  function mediumTex(){
+    sprite.loadTexture('enemy');
+  }
+  function largeTexture(){
+    sprite.loadTexture('enemyLarge');
+  }
   sprite.inputEnabled = true;
   sprite.events.onInputDown.add(destroySprite, this);
-  //bot.animations.add('run');
+  setTimeout(smallTex, 1000);
+  setTimeout(mediumTex, 3000);
+  setTimeout(largeTexture, 5000);
 }
 
 function requestLock() {
@@ -124,18 +151,16 @@ function update() {
 
 function destroySprite (sprite) {
   sprite.destroy();
+  counter++;
+  console.log(counter);
 }
 
 function clearDeadEnemies(){
   enemies = enemies.filter(sprite => sprite.alive);
-  console.log(enemies);
 }
 
-
 function start() {
-
-    BGMusic.fadeIn(1000);
-
+  BGMusic.fadeIn(1000);
 }
 
 function moveEmulation(numX, numY){
